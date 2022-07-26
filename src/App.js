6e63938fbe0404421bf84beb3fb87544a73d6e9c
Bae Sujeong <doya60@gmail.com>
2022-07-26 16:37:30 +0900

    셋팅, 메인페이지 작업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data.js";
 
 function App() {
+  const path = process.env.PUBLIC_URL;
+  const [Shoes, setShoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="navbar">
+        <Container>
+          <Navbar.Brand href="#home">SJ-Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          <Col>
+            <img src={`${path}/img/item1.jpg`} width="80%" className="mb20" />
+            <h4>{Shoes[0].title}</h4>
+            <p>{Shoes[0].price}</p>
+          </Col>
+          <Col>
+            <img src={`${path}/img/item2.jpg`} width="80%" className="mb20" />
+            <h4>{Shoes[1].title}</h4>
+            <p>{Shoes[1].price}</p>
+          </Col>
+          <Col>
+            <img src={`${path}/img/item3.jpg`} width="80%" className="mb20" />
+            <h4>{Shoes[2].title}</h4>
+            <p>{Shoes[2].price}</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+let data = [
+  {
+    id: 0,
+    title: "나이키 에어맥스 코코",
+    content: "여성 샌들",
+    price: 107100,
+  },
+
+  {
+    id: 1,
+    title: "나이키 에어맥스 97",
+    content: "남성 신발",
+    price: 179100,
+  },
+
+  {
+    id: 2,
+    title: "나이키 에어 리프트 브리드",
+    content: "여성 신발",
+    price: 116100,
+  },
+];
+
+export default data;
