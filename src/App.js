6f8a6b8d761cc93a2d7eb7024eb80f4a594c52a1
Bae Sujeong <doya60@gmail.com>
2022-07-26 22:51:33 +0900

    라우터 셋팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import { Navbar, Container, Nav, Row } from "react-bootstrap";
 import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import data from "./data.js";
 import Item from "./components/Item";
+import Detail from "./components/Detail";
 
 function App() {
   const path = process.env.PUBLIC_URL;
@@ -20,15 +22,26 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-
-      <Container>
-        <Row>
-          {Shoes.map((a, i) => {
-            return <Item path={path} Shoes={Shoes[i]} idx={i} />;
-          })}
-        </Row>
-      </Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {Shoes.map((a, i) => {
+                    return (
+                      <Item path={path} Shoes={Shoes[i]} idx={i} key={i} />
+                    );
+                  })}
+                </Row>
+              </Container>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
