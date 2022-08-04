f3835781f57e0ee88984ad505896ec00335e6754
Bae Sujeong <doya60@gmail.com>
2022-08-04 16:00:06 +0900

    sm={4} 로 스타일 적용

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Item(props) {
   return (
-    <Col className="col-1-3">
+    <Col sm={4}>
       <Link to={`/detail/${props.idx}`}>
         <img
           src={`${props.path}/img/item${props.idx}.jpg`}
